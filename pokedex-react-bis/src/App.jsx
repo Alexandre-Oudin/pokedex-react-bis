7937a4feb97663926e8a8534af0aba20278806d2
Alexandre OUDIN <alex.o51@hotmail.com>
2023-03-30 11:07:54 +0200

    react destructurer

--- pokedex-react-bis/src/App.jsx
+++ pokedex-react-bis/src/App.jsx
@@ -1,16 +1,35 @@
 import PokemonCard from './components/PokemonCard'
 import './App.css'
 
+ 
+//the array
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
+
+
+//the function
 function App() {
-  
-
+  let pokemon = pokemonList[0];
   return (
-    <div className="App">
-      
-    <PokemonCard />
+    <div className='App'>
 
+    <PokemonCard pokemon={pokemon} />
+      
     </div>
-  );
+  )
+
 }
+
+
 
 export default App
--- pokedex-react-bis/src/components/PokemonCard.jsx
+++ pokedex-react-bis/src/components/PokemonCard.jsx
@@ -1,31 +1,15 @@
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+function  PokemonCard ({pokemon}) {
 
-
-
-
-function  PokemonCard () {
-
-    const pokemon = pokemonList[0]
-    
 return(
 <figure>
+    
     <h1>PokemonCards</h1>
     {pokemon.imgSrc === undefined ? <p>???</p> :
-    
-    <img src={pokemon.imgSrc} alt={pokemon.name}></img>}
+    <img src={pokemon.imgSrc} alt={pokemon.name} />}
     <figcaption> {pokemon.name} </figcaption>
-</figure>
 
+</figure>
 );
 
  }
